32954391bc2fdcc47c3b42f8d7283e91826684d6
Akash Joshi <>
2020-05-22 11:06:04 +0530

    Update Uploader + Change SVG color

--- src/index.js
+++ src/index.js
@@ -2,22 +2,25 @@ import React from "react";
 import axios from "axios";
 
 import "./styles.css";
-import Camera from "./camera.svg";
 
-export default function FileUpload() {
+export default function FileUpload({
+  title,
+  subtitle,
+  setUrl,
+  required,
+  endpoint
+}) {
   const [uploadingStatus, setUploadingStatus] = React.useState(0);
   const [error, setError] = React.useState("");
-  const [url, setUrl] = React.useState("");
-
-  const copyToClipboard = str => navigator.clipboard.writeText(str);
 
   const uploadFile = e => {
+    console.log("uploading");
     const file = e.target.files;
     setUploadingStatus(1);
     const formData = new FormData();
     formData.append("file", file[0]);
     axios
-      .post(`/s3/upload`, formData, {
+      .post(endpoint, formData, {
         headers: {
           "Content-Type": "multipart/form-data"
         }
@@ -28,103 +31,97 @@ export default function FileUpload() {
       })
       .catch(error => {
         setUploadingStatus(-1);
-        e.target.value = "";
-        setError(`Error: ${error.response.data.msg}`);
+        document.querySelector("#react-s3-file-uploader").value = "";
+        setError(
+          `Error: ${
+            error.response.data.msg
+              ? error.response.data.msg
+              : "Could not connect to the Server."
+          }`
+        );
       });
   };
 
   return (
     <div>
-      <div
+      <label
+        htmlFor={`react-s3-file-uploader`}
         style={{
           border: "1.3px dashed #CAD2EA",
           borderRadius: "16px",
           padding: "1em",
           display: "grid",
-          gridTemplateColumns: "6.7em 1fr",
+          gridTemplateColumns: "8.5em 1fr",
           background: "white"
         }}
       >
-        <div>
-          <label htmlFor={`file_uploader`} style={{ width: "100%" }}>
-            <div
-              style={{
-                border: "1px solid rgba(0, 0, 0, 0.1)",
-                borderRadius: "16px",
-                padding: "1em",
-                backgroundColor: "#EFF3FC",
-                marginRight: "1em",
-                height: "5em",
-                textAlign: "center"
-              }}
-            >
-              <Camera />
-            </div>
-          </label>
+        <div
+          style={{
+            border: "1px solid rgba(0, 0, 0, 0.1)",
+            borderRadius: "16px",
+            padding: "1em",
+            backgroundColor: "#EFF3FC",
+            marginRight: "1em",
+            height: "5em",
+            width: "5em",
+            textAlign: "center"
+          }}
+        >
+          <img
+            src="https://raw.githubusercontent.com/akash-joshi/react-s3-file-uploader/master/src/camera.svg"
+            alt="upload-icon"
+            style={{ height: "2.5em", paddingTop: "20%" }}
+          />
         </div>
         <div>
-          <b style={{ fontSize: "18px" }}>Upload File</b>
+          <b style={{ fontSize: "18px" }}>{title}</b>
           <p style={{ color: "#999", marginTop: "0em", marginBottom: "0.1em" }}>
-            Click to upload
+            {subtitle}
           </p>
         </div>
-      </div>
+      </label>
 
       <input
         className="browser-default"
         style={{
-          border: "2px solid #dfdfdf",
-          padding: "0.7em",
-          borderRadius: "8px",
-          color: "#999",
-          width: "100%",
-          display: "none"
+          opacity: 0,
+          position: "absolute",
+          zIndex: -1
         }}
-        placeholder={"Upload Linking Image"}
         type="file"
         onChange={uploadFile}
-        required
-        id={`file_uploader`}
+        required={required}
+        id={`react-s3-file-uploader`}
       />
       {uploadingStatus === 1 && (
         <div
           className="valign-wrapper"
-          style={{ color: "#5e83e1", fontWeight: 600, marginTop: "0.7em" }}
+          style={{
+            color: "#5e83e1",
+            fontWeight: 600,
+            marginTop: -15,
+            marginLeft: -20
+          }}
         >
-          <div
-            style={{ marginRight: "0.7em" }}
-            className="preloader-wrapper small active"
-          >
-            <div className="spinner-layer spinner-blue-only">
-              <div className="circle-clipper left">
-                <div className="circle" />
-              </div>
-              <div className="gap-patch">
-                <div className="circle" />
-              </div>
-              <div className="circle-clipper right">
-                <div className="circle" />
-              </div>
-            </div>
+          <div class="loader">
+            <svg class="circular">
+              <circle
+                class="path"
+                fill="none"
+                r="20"
+                cx="50"
+                cy="50"
+                stroke-width="3"
+                stroke-miterlimit="10"
+              />
+            </svg>
           </div>
-          Uploading
+          <div>Uploading</div>
         </div>
       )}
       {uploadingStatus === 2 && (
-        <div>
-          <div style={{ color: "#08BD80", marginTop: "0.7em" }}>
-            File Uploaded ✓
-          </div>
-          <br />
-          URL: {url}
-          <br />
-          <br />
-          <a
-            style={{ color: "blue", cursor: "pointer", fontSize: 20 }}
-            onClick={() => copyToClipboard(url)}
-          >
-            Copy to Clipboard
-          </a>
+        <div style={{ color: "#08BD80", marginTop: "0.7em" }}>
+          File Uploaded â
         </div>
       )}
       {uploadingStatus === -1 && (
